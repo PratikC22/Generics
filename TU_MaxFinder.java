7721993f432493eeb8e983baa7e4098eecdde8e3
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-19 15:40:03 +0530

    Used 3 test cases

--- TU_MaxFinder.java
+++ TU_MaxFinder.java
@@ -1,5 +1,5 @@
 /**
- * This code tests whether maximum floats among three is expected or not.
+ * This code tests whether maximum String among three is expected or not.
  *
  * @author Pratik chaudhari
  * @since 19/06/2021
@@ -10,10 +10,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TU_MaxFinder {
-    @Test   // Testing whether code outputs expected maximum float or not.
-    void WhenFloatShouldReturnMax() {
-        Float float1 = 0.1f, float2 = 4.0f, float3 = 3.2f;
-        Float max = MaxFinder.findMax(float1, float2, float3);
-        Assertions.assertEquals(4.0f,max,"Validating max method");
+    @Test   // Maximum string at position 1.
+    void WhenString1ShouldReturnMax() {
+        String string1 = "Peach", string2 = "Lychee", string3 = "Guava";
+        String max = MaxFinder.findMax(string1, string2, string3);
+        Assertions.assertEquals("Peach",max,"Validating max method");
+    }
+    @Test   // Maximum string at position 2.
+    void WhenString2ShouldReturnMax() {
+        String string1 = "Lychee", string2 = "Peach", string3 = "Guava";
+        String max = MaxFinder.findMax(string1, string2, string3);
+        Assertions.assertEquals("Peach",max,"Validating max method");
+    }
+    @Test   // Maximum string at position 3.
+    void WhenString3ShouldReturnMax() {
+        String string1 = "Guava", string2 = "Lychee", string3 = "Peach";
+        String max = MaxFinder.findMax(string1, string2, string3);
+        Assertions.assertEquals("Peach",max,"Validating max method");
     }
 }
