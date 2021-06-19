84fb1bd3cb257af31e8d786aaf0efd8e2867a4e9
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-19 15:29:58 +0530

    Found maximum of string in given three strings

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,27 +1,27 @@
 /**
- * This code tests and checks maximum floats among three floats.
+ * This code tests and checks maximum String among three Strings.
  *
  * @author Pratik Chaudhari
  * @since 19/06/2021
  */
 package com.pratik.max;
 
+import java.util.Scanner;
+//Utilising compareTo method to find largest String among three.
 public class MaxFinder {
-    public static Float findMax(Float float1, Float float2, Float float3) {
-        Float max = float1;
-        //Utilising compareTo method to find largest float among three.
-        if (float2.compareTo(max)>0) {
-            max = float2;
-        }
-        if (float3.compareTo(max)>0) {
-            max = float3;
-        }
-        System.out.printf("Max value of %s %s %s is : %s\n", float1, float2, float3, max);
+    public static String findMax(String string1, String string2, String string3) {
+        String max = string1;
+        if(string2.compareToIgnoreCase(max) > 0)
+            max = string2;
+        if(string3.compareToIgnoreCase(max) > 0)
+            max = string3;
+        System.out.printf("Max value of %s %s %s is %s. \n", string1, string2, string3, max);
+
         return max;
     }
     // Main method
     public static void main(String[] args) {
-        Float float1 = 15.2f, float2 = 23.5f, float3 = 3.39f;
-        Float max = findMax(float1, float2, float3);
+        String string1 = "apple", string2 = "banana", string3 = "orange";
+        findMax(string1, string2, string3);
     }
 }
