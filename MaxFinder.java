c8ce93eea695a67f7428bb9641ae43be4dba8fb3
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-26 08:23:38 +0530

    Refactored to create one generics class

--- MaxFinder.java
+++ MaxFinder.java
@@ -7,20 +7,65 @@
  */
 package com.pratik.max;
 
-import java.util.Scanner;
+public class MaxFinder <T extends Comparable<T>> {
+    T num1;
+    T num2;
+    T num3;
 
-public class MaxFinder {
-    // Main method
-    public static void main(String[] args) {
-
-        System.out.println("Maximum Value of Integer given is : " + GenericMax(6, 3, 9));
-        System.out.println("Maximum value of Float numbers given is : " + GenericMax(1.0f, 5.0f, 3.0f));
-        System.out.println("Maximum value of String given is : " + GenericMax("Apple", "Peach", "Banana"));
-        System.out.println("Maximum value of String given is : " + GenericMax("Apple", "Peach", "Banana"));
-
+    public MaxFinder(T num1, T num2, T num3) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
     }
 
-    // Generic method
+    public MaxFinder() {
+    }
+
+    /**
+     * UC 1: method for finding maximum integer value
+     */
+    public static Integer findMax(Integer num1, Integer num2, Integer num3) {
+        Integer max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        return max;
+    }
+
+    /**
+     * UC 2:  method for finding maximum float value
+     */
+    public static Float findMax(Float num1, Float num2, Float num3) {
+        Float max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        return max;
+    }
+
+    /**
+     * UC 3:  method for finding maximum String value
+     */
+    public static String findMax(String str1, String str2, String str3) {
+        String max = str1;
+        if (str2.compareTo(max) > 0) {
+            max = str2;
+        }
+        if (str3.compareTo(max) > 0) {
+            max = str3;
+        }
+        return max;
+    }
+
+    /**
+     * using generics find Maximum value of String
+     */
     public static <T extends Comparable<T>> T GenericMax(T num1, T num2, T num3) {
         T max = num1;
         if (num2.compareTo(max) > 0)
@@ -30,33 +75,15 @@ public class MaxFinder {
         return max;
     }
 
-    public static Integer findMax(Integer num1, Integer num2, Integer num3) {
-        Integer max = num1;
-        if (num2.compareTo(max) > 0)
-            max = num2;
-        if (num3.compareTo(max) > 0)
-            max = num3;
-        return max;
+    public T GenericMax() {
+        return MaxFinder.GenericMax(num1, num2, num3);
     }
 
-    public static Float findMax(Float float1, Float float2, Float float3) {
-        Float max = float1;
-        if (float2.compareTo(max) > 0) {
-            max = float2;
-        }
-        if (float3.compareTo(max) > 0) {
-            max = float3;
-        }
-
-        return max;
-    }
-
-    public static String findMax(String string1, String string2, String string3) {
-        String max = string1;
-        if (string2.compareToIgnoreCase(max) > 0)
-            max = string2;
-        if (string3.compareToIgnoreCase(max) > 0)
-            max = string3;
-        return max;
+    public static void main(String[] args) {
+        System.out.println("Maximum Value of Integer given is :" + findMax(6, 3, 9));
+        System.out.println("Maximum value of Float numbers given is :" + findMax(1.0f, 5.0f, 3.0f));
+        System.out.println("Maximum value of String given is :" + findMax("Boy", "Girl", "Animal"));
+        System.out.println("Maximum value of String given is :" + GenericMax("Boy", "Girl", "Animal"));
+        System.out.println("Maximum Integer by creating Generic class :" + new MaxFinder<Integer>(3, 6, 2).GenericMax());
     }
 }
