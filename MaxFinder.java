472c68eb57843d930f8cb58b756d5a34a2d4d615
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-26 08:13:35 +0530

    Refactored code for generic method and used test cases

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,5 +1,6 @@
 /**
- * This code tests and checks maximum String among three Strings.
+ * This code tests and checks maximum among given inputs.
+ * supports int, float, string
  *
  * @author Pratik Chaudhari
  * @since 19/06/2021
@@ -7,21 +8,55 @@
 package com.pratik.max;
 
 import java.util.Scanner;
-//Utilising compareTo method to find largest String among three.
+
 public class MaxFinder {
-    public static String findMax(String string1, String string2, String string3) {
-        String max = string1;
-        if(string2.compareToIgnoreCase(max) > 0)
-            max = string2;
-        if(string3.compareToIgnoreCase(max) > 0)
-            max = string3;
-        System.out.printf("Max value of %s %s %s is %s. \n", string1, string2, string3, max);
+    // Main method
+    public static void main(String[] args) {
+
+        System.out.println("Maximum Value of Integer given is : " + GenericMax(6, 3, 9));
+        System.out.println("Maximum value of Float numbers given is : " + GenericMax(1.0f, 5.0f, 3.0f));
+        System.out.println("Maximum value of String given is : " + GenericMax("Apple", "Peach", "Banana"));
+        System.out.println("Maximum value of String given is : " + GenericMax("Apple", "Peach", "Banana"));
+
+    }
+
+    // Generic method
+    public static <T extends Comparable<T>> T GenericMax(T num1, T num2, T num3) {
+        T max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+        if (num3.compareTo(max) > 0)
+            max = num3;
+        return max;
+    }
+
+    public static Integer findMax(Integer num1, Integer num2, Integer num3) {
+        Integer max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+        if (num3.compareTo(max) > 0)
+            max = num3;
+        return max;
+    }
+
+    public static Float findMax(Float float1, Float float2, Float float3) {
+        Float max = float1;
+        if (float2.compareTo(max) > 0) {
+            max = float2;
+        }
+        if (float3.compareTo(max) > 0) {
+            max = float3;
+        }
 
         return max;
     }
-    // Main method
-    public static void main(String[] args) {
-        String string1 = "apple", string2 = "banana", string3 = "orange";
-        findMax(string1, string2, string3);
+
+    public static String findMax(String string1, String string2, String string3) {
+        String max = string1;
+        if (string2.compareToIgnoreCase(max) > 0)
+            max = string2;
+        if (string3.compareToIgnoreCase(max) > 0)
+            max = string3;
+        return max;
     }
 }
--- TU_MaxFinder.java
+++ TU_MaxFinder.java
@@ -10,22 +10,75 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TU_MaxFinder {
-    @Test   // Maximum string at position 1.
+    @Test
+        // Maximum string at position 1.
     void WhenString1ShouldReturnMax() {
         String string1 = "Peach", string2 = "Lychee", string3 = "Guava";
         String max = MaxFinder.findMax(string1, string2, string3);
-        Assertions.assertEquals("Peach",max,"Validating max method");
+        Assertions.assertEquals("Peach", max, "Validating max method");
     }
-    @Test   // Maximum string at position 2.
+
+    @Test
+        // Maximum string at position 2.
     void WhenString2ShouldReturnMax() {
         String string1 = "Lychee", string2 = "Peach", string3 = "Guava";
         String max = MaxFinder.findMax(string1, string2, string3);
-        Assertions.assertEquals("Peach",max,"Validating max method");
+        Assertions.assertEquals("Peach", max, "Validating max method");
     }
-    @Test   // Maximum string at position 3.
+
+    @Test
+        // Maximum string at position 3.
     void WhenString3ShouldReturnMax() {
         String string1 = "Guava", string2 = "Lychee", string3 = "Peach";
         String max = MaxFinder.findMax(string1, string2, string3);
-        Assertions.assertEquals("Peach",max,"Validating max method");
+        Assertions.assertEquals("Peach", max, "Validating max method");
+    }
+
+    @Test
+        // Testing whether code outputs expected maximum float or not.
+    void WhenFloat1ShouldReturnMax() {
+        Float float1 = 0.1f, float2 = 4.0f, float3 = 3.2f;
+        Float max = MaxFinder.findMax(float1, float2, float3);
+        Assertions.assertEquals(4.0f, max, "Validating max method");
+    }
+
+    @Test
+        // Testing whether code outputs expected maximum float or not.
+    void WhenFloat2ShouldReturnMax() {
+        Float float1 = 4.0f, float2 = 0.1f, float3 = 3.2f;
+        Float max = MaxFinder.findMax(float1, float2, float3);
+        Assertions.assertEquals(4.0f, max, "Validating max method");
+    }
+
+    @Test
+        // Testing whether code outputs expected maximum float or not.
+    void WhenFloat3ShouldReturnMax() {
+        Float float1 = 0.1f, float2 = 3.2f, float3 = 4.0f;
+        Float max = MaxFinder.findMax(float1, float2, float3);
+        Assertions.assertEquals(4.0f, max, "Validating max method");
+    }
+
+    @Test
+        // Testing whether code outputs expected maximum integer or not.
+    void WhenInteger1ShouldReturnMax() {
+        Integer int1 = 4, int2 = 1, int3 = 3;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+        Assertions.assertEquals(4, max, "Validating max method");
+    }
+
+    @Test
+        // Testing whether code outputs expected maximum integer or not.
+    void WhenInteger2ShouldReturnMax() {
+        Integer int1 = 1, int2 = 4, int3 = 3;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+        Assertions.assertEquals(4, max, "Validating max method");
+    }
+
+    @Test
+        // Testing whether code outputs expected maximum integer or not.
+    void WhenInteger3ShouldReturnMax() {
+        Integer int1 = 1, int2 = 3, int3 = 4;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+        Assertions.assertEquals(4, max, "Validating max method");
     }
 }
